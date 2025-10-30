fb8653faf963fc39a0e081d04fe4814bcce4263c
abcdefg-ab1 <3472361948@qq.com>
2025-10-30 19:32:41 +0800

    feat: update main.c in dev (add branch info)

--- main.c
+++ main.c
@@ -3,4 +3,8 @@ int main() {
     printf("Git code commit demo for OS Lab1\n");
     printf("Remote repo: git@github.com:abcdefg-ab1/abcdefg.git\n");
     return 0;
+    printf("当前分支：dev（开发分支）\n");
+    printf("修改内容：添加分支管理演示信息\n");
+    return 0;
 }
+
